f8e42bfdf8745b78919365a4ebb630d7d4f15173
Gregor Weiss <gregor.weiss@hlrs.de>
2024-01-10 19:51:20 +0100

    STYLE: remove pyFOAM related stuff for clean repository

--- foamPython/scriptProfileCode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Note: This script used to work when the case setup was in this folder.
-# To use, it should be added to case folders and modified accordingly
-
-python3 -m cProfile -s cumulative simpleFoam.py > profileLog
