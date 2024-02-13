e97c7aff6330f1a10a0e46a71f3578dc8ee1217f
Gregor Weiss <gregor.weiss@hlrs.de>
2024-02-13 18:21:06 +0100

    extend setting for Z dimension

--- heatTransfer/write/Settings.cpp
+++ heatTransfer/write/Settings.cpp
@@ -49,35 +49,37 @@ Settings::Settings(int argc, char *argv[], int rank, int nproc) : rank{rank}
     format = argv[3];
     npx = convertToUint("N", argv[4]);
     npy = convertToUint("M", argv[5]);
-    ndx = convertToUint("nx", argv[6]);
-    ndy = convertToUint("ny", argv[7]);
-    steps = convertToUint("steps", argv[8]);
-    iterations = convertToUint("iterations", argv[9]);
+    npz = convertToUint("M", argv[6]);
+    ndx = convertToUint("nx", argv[7]);
+    ndy = convertToUint("ny", argv[8]);
+    ndz = convertToUint("nz", argv[9]);
+    steps = convertToUint("steps", argv[10]);
+    iterations = convertToUint("iterations", argv[11]);
 
-    if ( argc >= 11 ) {
-        if ( std::string(argv[10]) == std::string("read") ) {
+    if ( argc >= 13 ) {
+        if ( std::string(argv[12]) == std::string("read") ) {
             read = true;
         }
-        if ( std::string(argv[10]) == std::string("remove") ) {
+        if ( std::string(argv[12]) == std::string("remove") ) {
             remove = true;
         }
     }
 
-    if ( argc >= 12 ) {
-        if ( std::string(argv[10]) == std::string("read")
+    if ( argc >= 14 ) {
+        if ( std::string(argv[12]) == std::string("read")
              ||
-             std::string(argv[11]) == std::string("read")) {
+             std::string(argv[13]) == std::string("read")) {
             read = true;
         }
-        if ( std::string(argv[10]) == std::string("remove")
+        if ( std::string(argv[12]) == std::string("remove")
              ||
-             std::string(argv[11]) == std::string("remove")) {
+             std::string(argv[13]) == std::string("remove")) {
             remove = true;
         }
     }
 
 
-    if (npx * npy != this->nproc)
+    if (npx * npy * npz != this->nproc)
     {
         throw std::invalid_argument("N*M must equal the number of processes");
     }
@@ -85,16 +87,21 @@ Settings::Settings(int argc, char *argv[], int rank, int nproc) : rank{rank}
     // calculate global array size and the local offsets in that global space
     gndx = npx * ndx;
     gndy = npy * ndy;
-    double global_bytes = static_cast<double>(gndx) * static_cast<double>(gndy) * sizeof(double);
-    double local_bytes  = static_cast<double>(ndx)  * static_cast<double>(ndy)  * sizeof(double);
+    gndz = npz * ndz;
+    double global_bytes = static_cast<double>(gndx) * static_cast<double>(gndy) *
+                          static_cast<double>(gndz) * sizeof(double);
+    double local_bytes  = static_cast<double>(ndx) * static_cast<double>(ndy) *
+                          static_cast<double>(ndz) * sizeof(double);
     localGB   = local_bytes  / std::pow( 10.0, 9 ) * static_cast<double>( iterations );
     globalGB  = global_bytes / std::pow( 10.0, 9 ) * static_cast<double>( iterations );
     localGiB  = local_bytes  / std::pow( 2.0, 30 ) * static_cast<double>( iterations );
     globalGiB = global_bytes / std::pow( 2.0, 30 ) * static_cast<double>( iterations );
     posx = rank % npx;
-    posy = rank / npx;
+    posy = ( rank / npx ) % npy;
+    posz = rank / (npx * npy);
     offsx = posx * ndx;
     offsy = posy * ndy;
+    offsz = posz * ndz;
 
     // determine neighbors
     if (posx == 0)
@@ -116,4 +123,14 @@ Settings::Settings(int argc, char *argv[], int rank, int nproc) : rank{rank}
         rank_right = -1;
     else
         rank_right = rank + npx;
+
+    if (posz == 0)
+        rank_front = -1;
+    else
+        rank_front = rank - (npx * npy);
+
+    if (posz == npz - 1)
+        rank_back = -1;
+    else
+        rank_back = rank + (npx * npy);
 }
--- heatTransfer/write/Settings.h
+++ heatTransfer/write/Settings.h
@@ -19,30 +19,36 @@ struct Settings
     std::string configfile;
     std::string outputfile;
     std::string format;
-    unsigned int npx;        // Number of processes in X (slow) dimension
-    unsigned int npy;        // Number of processes in Y (fast) dimension
+    unsigned int npx;        // Number of processes in X dimension
+    unsigned int npy;        // Number of processes in Y dimension
+    unsigned int npz;        // Number of processes in Z dimension
     unsigned int ndx;        // Local array size in X dimension per process
     unsigned int ndy;        // Local array size in y dimension per process
+    unsigned int ndz;        // Local array size in z dimension per process
     unsigned int steps;      // Number of output steps
     unsigned int iterations; // Number of computing iterations between steps
     bool read{ false };      // Switch to turn on re-reading
     bool remove{ false };    // Switch to turn on removal
 
     // calculated values from those arguments and number of processes
-    unsigned int gndx; // Global array size in slow dimension
-    unsigned int gndy; // Global array size in fast dimension
+    unsigned int gndx; // Global array size in X dimension
+    unsigned int gndy; // Global array size in Y dimension
+    unsigned int gndz; // Global array size in Z dimension
     double localGB;    // Local array size in GB
     double globalGB;   // Global array size in GB
     double localGiB;    // Local array size in GiB
     double globalGiB;   // Global array size in GiB
     // X dim positions: rank 0, npx, 2npx... are in the same X position
     // Y dim positions: npx number of consecutive processes belong to one row
+    // Z dim positions: npx*npy number of consecutive processes belong to one depth layer 
     // (npx
     // columns)
     unsigned int posx;  // Position of this process in X dimension
     unsigned int posy;  // Position of this process in Y dimension
+    unsigned int posz;  // Position of this process in Z dimension
     unsigned int offsx; // Offset of local array in X dimension on this process
     unsigned int offsy; // Offset of local array in Y dimension on this process
+    unsigned int offsz; // Offset of local array in Z dimension on this process
 
     int rank;           // MPI rank
     unsigned int nproc; // number of processors
@@ -52,6 +58,8 @@ struct Settings
     int rank_right;
     int rank_up;
     int rank_down;
+    int rank_front;
+    int rank_back;
 
     /** true: std::async Write, false (default): sync */
     bool async = false;
